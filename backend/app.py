234494619fe5f14843b0e15f46558cfa15d0bb91
Luca Colussi <luca.colussi@gmail.com>
2025-09-28 15:54:51 +0200

    feat: add categories and all notes

--- backend/app.py
+++ backend/app.py
@@ -14,9 +14,25 @@ def save_notes(note):
         json.dump(note, f, ensure_ascii=False, indent=2)
 
 @app.route("/notes", methods=["GET"])
-def get_todos():
+def get_notes():
     return jsonify(load_notes())
 
+@app.route("/notes/category", methods=["GET"])
+def get_notes_categories():
+    notes = load_notes()
+    result = [];
+    for n in notes:
+        if not isCategoryPresent(n, result):
+            result.append(n)
+    return jsonify(result)
+
+
+def isCategoryPresent(n, result):
+    for tmp in result:
+        if tmp["category"] == n["category"]:
+            return True
+    return False
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,39 @@
-async function load_notes(){
-    const res = await fetch("http://127.0.0.1:5000/notes");
+async function load_categories_notes(){
+    const res = await fetch("http://127.0.0.1:5000/notes/category");
     const data = await res.json();
     console.log(data);
     const body = document.getElementById("categories")
     body.innerHTML = "";
-    data.forEach((note) =>{
+    data.forEach((note) =>{ // get category
         const div = document.createElement("div");
-        div.textContent= note.body;
+        div.textContent = note.category;
+        body.appendChild(div);
+    })
+}
+
+async function load_notes(){
+    const res = await fetch("http://127.0.0.1:5000/notes");
+    const data = await res.json();
+    const body = document.getElementById("list")
+    data.forEach((note) =>{ 
+        const div = document.createElement("div");
+        div.className = "note"
+        const title = document.createElement("p");
+        title.textContent = note.title;
+
+        const text = document.createElement("p");
+        text.textContent = note.body;
+
+        const category = document.createElement("p");
+        category.textContent = note.category;
+        title.className = "title";
+        div.appendChild(title);
+        div.appendChild(text);
+        div.appendChild(category);
+        
         body.appendChild(div);
     })
 }
 
 load_notes();
+load_categories_notes();
